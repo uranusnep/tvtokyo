d5d54cb891d71e6e635a6134bc8abd90602072bd
uranusnep <miruru211@outlook.jp>
2021-01-08 10:53:53 +0000

    01-08

---
+++ js/tvtokyo.js
@@ -0,0 +1,11 @@
+$(function(){
+    
+    $(".slider").slick({
+        autoplay:true,
+        dots:true,
+        arrows:true,
+        centerMode:true,
+        centerPadding:"15%",
+    });
+    
+});
